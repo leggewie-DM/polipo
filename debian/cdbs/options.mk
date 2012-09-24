88d32db3e1f59507da3fc843dae8e2c5ca9e0635
Rolf Leggewie <foss@rolf.leggewie.biz>
2012-09-24 12:09:06 +0200

    move over build system to dh7 from cdbs
    - use rules.tiny
    - drop build-dependency on cdbs
    - add polipo.dirs
    - explicitly install polipo-control script and options file
    - set executable bits for polipo-control script
    - drop cdbs helper scripts

--- debian/cdbs/options.mk
+++
@@ -1,9 +0,0 @@
-common-install-arch::
-	shopt nullglob; \
-	install --mode=644 -D debian/options $(DEB_DESTDIR)/etc/polipo/
-	mkdir -p $(DEB_DESTDIR)/var/cache/polipo
-	chown proxy:proxy $(DEB_DESTDIR)/var/cache/polipo
-	chmod 755 $(DEB_DESTDIR)/var/cache/polipo
-	mkdir -p $(DEB_DESTDIR)/var/log/polipo
-	chown proxy:adm $(DEB_DESTDIR)/var/log/polipo
-	chmod 2755 $(DEB_DESTDIR)/var/log/polipo
